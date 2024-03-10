59d64203ad50266deca0e8020427bca6745e519b
Jacob <24348787+0xJacobean@users.noreply.github.com>
2024-03-09 20:25:17 -0800

    lots of stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,8 +17,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Fully Automated Laissez-Faire Journalism",
+  description: "Automated news generation for internet natives",
 };
 
 export default function RootLayout({
