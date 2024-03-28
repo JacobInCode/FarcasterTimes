6b152cec83c18cd9b2aeedd80cd92f466b64d835
Jacob <24348787+0xJacobean@users.noreply.github.com>
2024-03-28 11:17:02 -0700

    adding steps to inngest generate article call

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -2,6 +2,8 @@ import { serve } from "inngest/next";
 import { inngest } from "@/app/inngest/client";
 import { generateChannelArticle } from "@/app/inngest/function";
 
+export const runtime = "edge";
+
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
