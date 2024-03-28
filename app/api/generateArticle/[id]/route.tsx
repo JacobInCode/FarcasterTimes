0de7bfff62bd19d7b240f191f001e98a213e77a1
Jacob <24348787+0xJacobean@users.noreply.github.com>
2024-03-28 12:35:42 -0700

    adding steps to inngest generate article call

--- app/api/generateArticle/[id]/route.tsx
+++ app/api/generateArticle/[id]/route.tsx
@@ -9,7 +9,7 @@ export async function GET(req: Request) {
 
   const channelId = req.url.split('/').pop();
 
-  console.log(`Generating articles for channel ${channelId}`);
+  console.log(`Generating article for channel ${channelId}`);
 
   // Send your event payload to Inngest
   await inngest.send({
