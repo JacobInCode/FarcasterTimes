9494ee7ee7adc45b5cca048cceca9fa5365be0d4
Jacob <24348787+0xJacobean@users.noreply.github.com>
2024-03-31 07:50:35 -0700

    seperating background task for gen citizen articles into steps

--- app/inngest/function.ts
+++ app/inngest/function.ts
@@ -1,7 +1,8 @@
-import { describeImage, fetchFeed, generateImage, getTopFiveArticles, organizeHashesByTopic, submitArticles, writeArticle, genCitizenArticle } from "@/lib/utils/fetch";
+import { describeImage, fetchFeed, generateImage, getTopFiveArticles, organizeHashesByTopic, submitArticles, writeArticle, genCitizenArticle, lookUpCastByHashOrWarpcastUrl, chooseChannelId } from "@/lib/utils/fetch";
 import { inngest } from "./client";
 import { formatArticleWithAuthorLinks, generateCitizenHTML, generateEmailHTML, parseArticleToJSON, parseJSONStringHashes } from "@/lib/utils/helpers";
-import { defaultUrl } from "@/lib/utils/config";
+import { channels, defaultUrl } from "@/lib/utils/config";
+import { CastResponse } from "@neynar/nodejs-sdk/build/neynar-api/v2";
 
 export const generateChannelArticle = inngest.createFunction(
     { id: "generate-article" },
@@ -83,11 +84,11 @@ export const generateChannelArticle = inngest.createFunction(
             const finalArticles = addedLinks.map((article: string) => parseArticleToJSON(article)).map((article: any, index: number) => {
                 return {
                     ...article,
-                    sources: topicallyOrganizedCasts[index].map((cast: any) => { 
+                    sources: topicallyOrganizedCasts[index].map((cast: any) => {
                         const likes = cast?.reactions?.likes?.length || 0;
                         const recasts = cast?.reactions?.recasts?.length || 0;
                         const replies = cast?.reactions?.replies?.count || 0;
-                        return { hash: cast.hash, username: cast.author.username, fid: cast.author.fid, likes, recasts, replies} 
+                        return { hash: cast.hash, username: cast.author.username, fid: cast.author.fid, likes, recasts, replies }
                     }),
                     channel_id: channelId,
                     citizen: false
@@ -132,11 +133,11 @@ export const sendEmails = inngest.createFunction(
         // find top 5 articles from last 24 hrs 
         const topFive = await getTopFiveArticles();
 
-        console.log(topFive);    
+        console.log(topFive);
 
         const emailData = {
             subject: "Citizen Times - Top 5 Articles",
-            text : "Here are the top 5 articles from Citizen Times over the last 24hrs.",
+            text: "Here are the top 5 articles from Citizen Times over the last 24hrs.",
             html: generateEmailHTML(topFive.map((article: any) => ({ headline: article.headline, body: article.body.slice(0, 75), id: article.id }))),
         };
 
@@ -152,7 +153,7 @@ export const sendEmails = inngest.createFunction(
                     },
                     body: JSON.stringify(emailData),
                 });
-    
+
                 const result = await response.json();
                 console.log(result);
             } catch (error) {
@@ -169,12 +170,94 @@ export const generateCitizenArticle = inngest.createFunction(
 
         console.log("GENERATING CITIZEN ARTICLE", event.data.urls);
 
-        const data = await genCitizenArticle(event.data.urls);
+        const urls = event.data.urls;
+
+        // const data = await genCitizenArticle(event.data.urls);
+
+        const castsRes = await step.run("look-up-cast", async () => {
+            // get casts from warpcast urls
+            const casts: CastResponse[] = await lookUpCastByHashOrWarpcastUrl(urls as string[]);
+            return casts;
+        })
+
+        const mappedCasts = castsRes.map(c => c.cast).map((cast: any) => cast)
+
+        const imageDescriptions = await step.run("generate-images", async () => {
+            // add image descriptions to any casts with images or frames
+            const imgDesc = await Promise.all(castsRes.map(c => c.cast).map((cast: any) => {
+
+                if (cast?.embeds[0]?.url && (cast.embeds[0]?.url.includes("png") || cast.embeds[0].url.includes("jpg") || cast.embeds[0].url.includes("jpeg") || cast.embeds[0].url.includes("gif"))) {
+                    return describeImage(cast.embeds[0].url)
+                } else if (cast?.embeds[1]?.url && (cast.embeds[1].url.includes("png") || cast.embeds[1].url.includes("jpg") || cast.embeds[1].url.includes("jpeg") || cast.embeds[1].url.includes("gif"))) {
+                    return describeImage(cast.embeds[1].url)
+                } else if (cast?.frames?.[0]?.image) {
+                    return describeImage(cast.frames[0].image)
+                } else {
+                    return Promise.resolve(null)
+                }
+
+            }));
+
+            return imgDesc;
+        });
+
+        const parsedArticle = await step.run("write-article", async () => {
+
+            const castsWithImageDescs = mappedCasts.map((cast: any, index: number) => {
+                return { ...cast, text: `CAST TEXT: ${cast.text}${!!imageDescriptions[index] ? "\n DESCRIPTION OF IMAGE INCLUDED IN CAST: " + imageDescriptions[index] : ""}` }
+            })
+
+            // write article
+            const articleRes = await writeArticle(JSON.stringify(castsWithImageDescs));
+            const article = parseArticleToJSON(formatArticleWithAuthorLinks(articleRes));
+
+            return article;
+        });
+
+
+        const finalArticleObject: any = await step.run("final-article", async () => {
+
+            // choose channel id from article text
+            const choosenChannelId = await chooseChannelId(JSON.stringify(channels.map(c => c.id)), `${parsedArticle.headline}\n\n${parsedArticle.body}`);
+
+            const _finalArticleObject = {
+                ...parsedArticle,
+                sources: mappedCasts.map((cast: any) => {
+                    const likes = cast?.reactions?.likes?.length || 0;
+                    const recasts = cast?.reactions?.recasts?.length || 0;
+                    const replies = cast?.reactions?.replies?.count || 0;
+                    return { hash: cast.hash, username: cast.author.username, fid: cast.author.fid, likes, recasts, replies }
+                }),
+                channel_id: channels.find(c => choosenChannelId.toLowerCase().includes(c.id.toLowerCase()))?.id || "",
+            };
+
+            return _finalArticleObject;
+        });
+
+        const image = await step.run("image-gen", async () => {
+            // generate image and speech
+            const image = await generateImage(`Create an vibrant image to describe this headline: ${finalArticleObject.headline}`);
+            // const audio = await generateSpeech(`${finalArticleObject.headline}\n\n${removeMarkdownLinks(finalArticleObject.body)}`);
+            return image;
+        });
+
+        const data = await step.run("submit-article", async () => {
+            // save article
+            const { data } = await submitArticles([{
+                ...finalArticleObject,
+                image: image.imageUrl,
+                // audio: audio.speechUrl,
+                citizen: true
+            }])
+
+            return data;
+        });
+
 
         const emailData = {
             recipient: event.data.email,
             subject: "Citizen Article Completed!",
-            text : "Your article has been generated successfully.",
+            text: "Your article has been generated successfully.",
             html: generateCitizenHTML(data[0]),
         };
 
@@ -192,7 +275,7 @@ export const generateCitizenArticle = inngest.createFunction(
                     },
                     body: JSON.stringify(emailData),
                 });
-    
+
                 const result = await response.json();
                 console.log(result);
             } catch (error) {
