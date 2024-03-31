1e7e7312e6a9b150502acc0a86dd91fc301cae45
Jacob <24348787+0xJacobean@users.noreply.github.com>
2024-03-31 07:16:39 -0700

    CT background task test

--- components/ReceiveEmail.tsx
+++ components/ReceiveEmail.tsx
@@ -106,11 +106,11 @@ const ReceiveEmail: React.FC<ReceiveEmailProps> = ({ children, urls, setUrls }) 
                             }}
                         />
                         <div className='flex space-x-2 w-full'>
-                            <Button disabled={!loading} variant="secondary" className="h-8 bg-black text-white w-full" onClick={() => fetchData(1)}>
+                            <Button disabled={!loading && value !== ""} variant="secondary" className="h-8 bg-black text-white w-full" onClick={() => fetchData(1)}>
                                 {loading === 1 && <Loader2Icon className="h-4 w-4 animate-spin mr-3" />}
                                 Receive Email
                             </Button>
-                            <Button disabled={!loading} variant="secondary" className="h-8 bg-black text-white w-full" onClick={() => fetchData(2)}>
+                            <Button disabled={!loading && value !== ""} variant="secondary" className="h-8 bg-black text-white w-full" onClick={() => fetchData(2)}>
                                 {loading === 2 && <Loader2Icon className="h-4 w-4 animate-spin mr-3" />}
                                 Don't Receive Email
                             </Button>
