517310a5a8eccb97acd8b7a81a2a674f287eea25
Jacob <24348787+0xJacobean@users.noreply.github.com>
2024-03-31 20:05:04 -0700

    twitter image update

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,9 @@ export default function RootLayout({
 
   return (
     <html lang="en" className={tinos.className}>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+      </head>
       <body className="bg-background text-foreground px-4">
         <main className="min-h-screen flex flex-col items-center">
           {children}
