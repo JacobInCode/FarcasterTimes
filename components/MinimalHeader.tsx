8e61a3f4c15457a6ba414c5b4e8bd69313d37c10
Jacob <24348787+0xJacobean@users.noreply.github.com>
2024-04-02 21:10:48 -0700

    update opengraph images

--- components/MinimalHeader.tsx
+++ components/MinimalHeader.tsx
@@ -5,8 +5,8 @@ import TitleLogo from './TitleLogo';
 const MinimalHeader: React.FC = () => {
     return (
         <div className='w-full flex flex-col items-center justify-center bg-background'>
-            <Link href="/" className='py-3 flex justify-center relative w-80 sm:w-96'>
-                <TitleLogo className="w-32" />
+            <Link href="/" className='py-3 md:py-4 flex justify-center relative w-80 sm:w-96'>
+                <TitleLogo className="w-32 md:w-64" />
             </Link>
             <div className="border-b w-full" />
         </div>
