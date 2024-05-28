ecf18d0c55ec577708719e74dd9c8fe550a2158c
Jacob <24348787+0xJacobean@users.noreply.github.com>
2024-05-28 11:28:11 -0700

    chore: Refactor email route to use Supabase client for authentication and remove unnecessary code

--- app/api/email/route.tsx
+++ app/api/email/route.tsx
@@ -1,3 +1,4 @@
+import { createBrowserClient } from '@supabase/ssr';
 import nodemailer from 'nodemailer';
 import * as z from "zod"
 
@@ -6,89 +7,23 @@ export const dynamic = "force-dynamic";
 
 const schema = z.object({
     recipient: z.string(),
-    file_id: z.string(),
+    subject: z.string(),
+    text: z.string(),
+    html: z.string(),
 });
-
-const getHtml = (file_id: string) => `<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <style>
-    body {
-      font-family: Arial, sans-serif;
-      margin: 0;
-      padding: 0;
-      background-color: #f4f4f4;
-    }
-    .container {
-      max-width: 600px;
-      margin: 20px auto;
-      background-color: #ffffff;
-      padding: 20px;
-      border-radius: 8px;
-      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    }
-    .header {
-      text-align: center;
-      padding-bottom: 20px;
-    }
-    .header h1 {
-      margin: 0;
-      color: #333333;
-    }
-    .content {
-      text-align: center;
-    }
-    .content p {
-      font-size: 16px;
-      color: #666666;
-      line-height: 1.5;
-    }
-    .content a {
-      display: inline-block;
-      margin-top: 20px;
-      padding: 10px 20px;
-      font-size: 16px;
-      color: #ffffff;
-      background-color: #007BFF;
-      text-decoration: none;
-      border-radius: 5px;
-    }
-    .content a:hover {
-      background-color: #0056b3;
-    }
-    .footer {
-      text-align: center;
-      margin-top: 20px;
-      font-size: 12px;
-      color: #999999;
-    }
-  </style>
-</head>
-<body>
-  <div class="container">
-    <div class="header">
-      <h1>Roneo Translation</h1>
-    </div>
-    <div class="content">
-      <p>Your translation is ready!</p>
-      <p>You can download it using the link below:</p>
-      <a href=${file_id} target="_blank">Download PDF</a>
-    </div>
-    <div class="footer">
-      <p>&copy; 2024 Roneo. All rights reserved.</p>
-    </div>
-  </div>
-</body>
-</html>`
 
 export async function POST(
     req: Request,
 ) {
+    const supabaseAdmin = createBrowserClient(process.env.NEXT_PUBLIC_SUPABASE_URL || '', process.env.SUPABASE_SERVICE_ROLE || '')
+
+    if (!supabaseAdmin) {
+        throw new Error('Not authenticated')
+    }
+
     const json = await req.json();
 
-    const { recipient, file_id } = schema.parse(json);
+    const { recipient, subject, text, html } = schema.parse(json);
 
     const transporter = nodemailer.createTransport({
         service: 'gmail',
@@ -98,16 +33,14 @@ export async function POST(
         },
     });
 
-    const subject = 'Roneo - Your translation is ready!';
-
     const sendEmail = (recipient: string) => {
         return new Promise((resolve, reject) => {
             const mailOptions = {
                 from: process.env.GMAIL_EMAIL,
                 to: recipient,
                 subject: subject,
-                text: `Roneo - Your translation is ready! ${file_id}`,
-                html: getHtml(file_id),
+                text: text,
+                html: html,
             };
 
             transporter.sendMail(mailOptions, (error: any, info: any) => {
